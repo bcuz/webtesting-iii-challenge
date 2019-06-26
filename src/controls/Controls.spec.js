3b148cb0b14fc8333a51085a89b55599aca2d51a
adam <bcuz@users.noreply.github.com>
2019-06-25 21:37:03 -0400

    rest of controls component tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -23,4 +23,43 @@ describe('<Controls />', () => {
 
 
   })
+  it('should be able to open the gate when its closed', () => {
+    let toggleClosed = jest.fn()
+
+    let { getByText } = render(<Controls
+    closed={true} toggleClosed={toggleClosed} />)
+ 
+    let button = getByText(/Open Gate/i)
+
+    fireEvent.click(button)
+
+    expect(toggleClosed).toBeCalled()
+
+  })
+  it('should be able to lock the gate when its unlocked and closed', () => {
+    let toggleLocked = jest.fn()
+
+    let { getByText } = render(<Controls
+    closed={true} locked={false} toggleLocked={toggleLocked} />)
+ 
+    let button = getByText(/Lock Gate/i)
+
+    fireEvent.click(button)
+
+    expect(toggleLocked).toBeCalled()
+
+  })
+  it('should be able to unlock the gate when its locked and closed', () => {
+    let toggleLocked = jest.fn()
+
+    let { getByText } = render(<Controls
+    closed={true} locked={true} toggleLocked={toggleLocked} />)
+ 
+    let button = getByText(/\bunLock Gate\b/i)
+
+    fireEvent.click(button)
+
+    expect(toggleLocked).toBeCalled()
+
+  })
 });
