3c39a8df40fe986de61c88d7248e3910782e1140
adam <bcuz@users.noreply.github.com>
2019-06-25 21:26:50 -0400

    close gate when its open

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer'; 
+import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each'
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+
+  it('should be able to close gate when its open', () => {
+    // testing that the function is being fired
+    let toggleClosed = jest.fn()
+
+    let { getByText } = render(<Controls
+    closed={false} toggleClosed={toggleClosed} />)
+ 
+    let button = getByText(/Close Gate/i)
+
+    fireEvent.click(button)
+
+    expect(toggleClosed).toBeCalled()
+
+
+  })
+});
